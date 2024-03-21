32dbd9e9ce637540a51ceddb81d7f449ee942573
alemi <me@alemi.dev>
2024-03-21 01:10:33 +0100

    fix: temp rename relationship field to avoid clash

--- src/activitystream/object/relationship.rs
+++ src/activitystream/object/relationship.rs
@@ -3,13 +3,15 @@ use crate::activitystream::Node;
 pub trait Relationship : super::Object {
 	fn subject(&self) -> Node<impl super::Object> { Node::Empty::<serde_json::Value> }
 	fn relationship(&self) -> Option<&str> { None } // TODO what does this mean???
-	fn object(&self) -> Node<impl super::Object> { Node::Empty::<serde_json::Value> }
+	// TODO was just object but clashes with Activity
+	fn relationship_object(&self) -> Node<impl super::Object> { Node::Empty::<serde_json::Value> }
 }
 
 pub trait RelationshipMut : super::ObjectMut {
 	fn set_subject(self, val: Node<impl super::Object>) -> Self;
 	fn set_relationship(self, val: Option<&str>) -> Self; // TODO what does this mean???
-	fn set_object(self, val: Node<impl super::Object>) -> Self;
+	// TODO was just object but clashes with Activity
+	fn set_relationship_object(self, val: Node<impl super::Object>) -> Self;
 }
 
 impl Relationship for serde_json::Value {
