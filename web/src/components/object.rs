8969780b29e83df1ea7621142179f55bc6b3c1a0
alemi <me@alemi.dev>
2024-04-23 23:28:19 +0200

    feat(web): added CWs, better images

--- web/src/components/object.rs
+++ web/src/components/object.rs
@@ -7,15 +7,23 @@ use apb::{target::Addressed, Base, Object};
 #[component]
 pub fn Object(object: serde_json::Value) -> impl IntoView {
 	let uid = object.id().unwrap_or_default().to_string();
-	let summary = object.summary().unwrap_or_default().to_string();
 	let content = dissolve::strip_html_tags(object.content().unwrap_or_default());
 	let author_id = object.attributed_to().id().unwrap_or_default();
 	let author = CACHE.get_or(&author_id, serde_json::Value::String(author_id.clone()));
 	let attachments = object.attachment()
 		.map(|x| view! {
-			<p class="center"><img class="attachment" src={x.url().id().unwrap_or_default()} title={x.name().unwrap_or_default().to_string()} /></p>
+			<p class="center">
+				<a href={x.url().id().unwrap_or_default()} target="_blank">
+					<img class="attachment" src={x.url().id().unwrap_or_default()} title={x.name().unwrap_or_default().to_string()} />
+				</a>
+			</p>
 		})
 		.collect_view();
+	let attachments_padding = if object.attachment().is_empty() {
+		None
+	} else {
+		Some(view! { <div class="pb-1"></div> })
+	};
 	view! {
 		<table class="align w-100">
 			<tr>
@@ -33,10 +41,27 @@ pub fn Object(object: serde_json::Value) -> impl IntoView {
 			</tr>
 		</table>
 		<blockquote class="tl">
-			{if summary.is_empty() { None } else { Some(view! { <code class="color ml-1">{summary}</code> })}}
-			{content.into_iter().map(|x| view! { <p>{x}</p> }).collect_view()}
+			<Summary summary=object.summary().map(|x| x.to_string()) >
+				{content.into_iter().map(|x| view! { <p>{x}</p> }).collect_view()}
+				{attachments_padding}
+				{attachments}
+			</Summary>
 		</blockquote>
-		{attachments}
+	}
+}
+
+#[component]
+pub fn Summary(summary: Option<String>, children: Children) -> impl IntoView {
+	match summary.filter(|x| !x.is_empty()) {
+		None => children().into_view(),
+		Some(summary) => view! {
+			<details class="pa-s">
+				<summary>
+					<code class="cw color ml-s w-100">{summary}</code>
+				</summary>
+				{children()}
+			</details>
+		}.into_view(),
 	}
 }
 
