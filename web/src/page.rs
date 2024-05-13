8ef6dca52bc0430a8f03e404399c95e4c51bb8a3
alemi <me@alemi.dev>
2024-05-13 02:53:34 +0200

    fix(web): better spacing for search

--- web/src/page.rs
+++ web/src/page.rs
@@ -410,33 +410,33 @@ pub fn SearchPage() -> impl IntoView {
 
 	view! {
 		<Breadcrumb>search</Breadcrumb>
-		<blockquote class="mt-1 mb-1">
+		<blockquote class="mt-3 mb-3">
 			<details open>
 				<summary class="mb-2">
 					<code class="cw center color ml-s w-100">users</code>
 				</summary>
+				<div class="pb-1">
 				{move || match user.get() {
-					None => None,
-					Some(None) => None,
-					Some(Some(u)) => Some(view! {
-						<ActorBanner object=u />
-					}),
+					None => view! { <p class="center"><small>searching...</small></p> },
+					Some(None) => view! { <p class="center"><code>N/A</code></p> },
+					Some(Some(u)) => view! { <p><ActorBanner object=u /></p> },
 				}}
+				</div>
 			</details>
 		</blockquote>
 
-		<blockquote class="mt-1 mb-1">
+		<blockquote class="mt-3 mb-3">
 			<details open>
 				<summary class="mb-2">
 					<code class="cw center color ml-s w-100">objects</code>
 				</summary>
+				<div class="pb-1">
 				{move || match object.get() {
-					None => None,
-					Some(None) => None,
-					Some(Some(o)) => Some(view! {
-						<Object object=o />
-					}),
+					None => view! { <p class="center"><small>searching...</small></p> },
+					Some(None) => view!{ <p class="center"><code>N/A</code></p> },
+					Some(Some(o)) => view! { <p><Object object=o /></p> },
 				}}
+				</div>
 			</details>
 		</blockquote>
 	}
