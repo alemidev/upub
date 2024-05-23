cd67863a47d60d059a1ed97c9d0a61a85cdf8b53
alemi <me@alemi.dev>
2024-05-23 05:53:05 +0200

    fix(web): tracing in web too

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-	_ = console_log::init_with_level(log::Level::Info);
 	console_error_panic_hook::set_once();
+
+	tracing_subscriber::fmt()
+		.with_writer(
+			tracing_subscriber_wasm::MakeConsoleWriter::default()
+				.map_trace_level_to(tracing::Level::DEBUG)
+		)
+		.with_ansi(false)
+		.without_time()
+		.init();
 
 	leptos::mount_to_body(upub_web::App);
 }
