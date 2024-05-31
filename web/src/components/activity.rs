6469dbe85e6caf33f4ed4b42d104385dad8de368
alemi <me@alemi.dev>
2024-05-31 15:54:22 +0200

    feat(web): filter updated, more readable filter code

--- web/src/components/activity.rs
+++ web/src/components/activity.rs
@@ -45,51 +45,39 @@ pub fn Item(
 	let config = use_context::<Signal<crate::Config>>().expect("missing config context");
 	let id = item.id().unwrap_or_default().to_string();
 	let sep = if sep { Some(view! { <hr /> }) } else { None };
-	match item.object_type() {
+	if !replies && !config.get().filters.visible(&item) {
+		return None;
+	}
+	match item.object_type().unwrap_or(apb::ObjectType::Object) {
 		// special case for placeholder activities
-		Some(apb::ObjectType::Note) | Some(apb::ObjectType::Document(_)) => (move || {
-			if !config.get().filters.replies && item.in_reply_to().id().is_some() {
-				None
-			} else if config.get().filters.orphans {
-				Some(view! { <Object object=item.clone() />{sep.clone()} })
-			} else {
-				None
-			}
-		}).into_view(),
+		apb::ObjectType::Note | apb::ObjectType::Document(_) =>
+			Some(view! { <Object object=item.clone() />{sep.clone()} }.into_view()),
 		// everything else
-		Some(apb::ObjectType::Activity(t)) => (move || {
-			if config.get().filters.visible(apb::ObjectType::Activity(t)) {
-				let object_id = item.object().id().unwrap_or_default();
-				if !replies && !config.get().filters.replies && CACHE.get(&object_id).map(|x| x.in_reply_to().id().is_some()).unwrap_or(false) {
-					None
-				} else {
-					let object = match t {
-						apb::ActivityType::Create | apb::ActivityType::Announce => 
-							CACHE.get(&object_id).map(|obj| {
-								view! { <Object object=obj /> }
-							}.into_view()),
-						apb::ActivityType::Follow =>
-							CACHE.get(&object_id).map(|obj| {
-								view! {
-									<div class="ml-1">
-										<ActorBanner object=obj />
-										<FollowRequestButtons activity_id=id.clone() actor_id=object_id />
-									</div>
-								}
-							}.into_view()),
-						_ => None,
-					};
-					Some(view! {
-						<ActivityLine activity=item.clone() />
-						{object}
-						{sep.clone()}
-					})
-				}
-			} else {
-				None
-			}
-		}).into_view(),
+		apb::ObjectType::Activity(t) => {
+			let object_id = item.object().id().unwrap_or_default();
+			let object = match t {
+				apb::ActivityType::Create | apb::ActivityType::Announce => 
+					CACHE.get(&object_id).map(|obj| {
+						view! { <Object object=obj /> }
+					}.into_view()),
+				apb::ActivityType::Follow =>
+					CACHE.get(&object_id).map(|obj| {
+						view! {
+							<div class="ml-1">
+								<ActorBanner object=obj />
+								<FollowRequestButtons activity_id=id.clone() actor_id=object_id />
+							</div>
+						}
+					}.into_view()),
+				_ => None,
+			};
+			Some(view! {
+				<ActivityLine activity=item.clone() />
+				{object}
+				{sep.clone()}
+			}.into_view())
+		},
 		// should never happen
-		_ => view! { <p><code>type not implemented</code></p> }.into_view(),
+		t => Some(view! { <p><code>type not implemented : {t.as_ref().to_string()}</code></p> }.into_view()),
 	}
 }
