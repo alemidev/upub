d6b9ab4cfdf4e08653b1e4678782b048611a23e6
alemi <me@alemi.dev>
2024-07-05 02:24:05 +0200

    fix(web): smaller actor summaries

--- web/src/actors/header.rs
+++ web/src/actors/header.rs
@@ -97,7 +97,7 @@ pub fn ActorHeader() -> impl IntoView {
 							}}
 						</div>
 					</div>
-					<p class="mb-2 mt-0 center" inner_html={mdhtml::safe_html(actor.summary().unwrap_or_default())}></p>
+					<p class="mb-2 mt-0 center bio" inner_html={mdhtml::safe_html(actor.summary().unwrap_or_default())}></p>
 				</div>
 				<Outlet />
 			}.into_view()
