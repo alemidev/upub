e8da3765ada21c471be1af649cb1648fc00e781b
alemi <me@alemi.dev>
2024-11-10 21:51:26 +0100

    fix(web): add fetch to feeds too if logged
    does nothing everywhere except for replies, maybe this is a bad design??
    no it's the good sense to be wrong!!

--- web/src/timeline/mod.rs
+++ web/src/timeline/mod.rs
@@ -84,6 +84,9 @@ impl Timeline {
 				feed_url + "?replies=false"
 			};
 		}
+		if auth.present() {
+			feed_url += "&fetch=true";
+		}
 		let collection : serde_json::Value = Http::fetch(&feed_url, auth).await?;
 		let activities : Vec<serde_json::Value> = collection
 			.ordered_items()
