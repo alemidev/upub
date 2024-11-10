ae85351b115a14108702e1aac379714cc011e6ae
alemi <me@alemi.dev>
2024-11-11 00:40:39 +0100

    fix(web): context and replies root id

--- web/src/objects/context.rs
+++ web/src/objects/context.rs
@@ -7,16 +7,18 @@ use crate::prelude::*;
 pub fn ObjectContext() -> impl IntoView {
 	let feeds = use_context::<Feeds>().expect("missing feeds context");
 	let params = use_params::<IdParam>();
+	let id = Signal::derive(move || {
+		params.get().ok().and_then(|x| x.id).unwrap_or_default()
+	});
 	create_effect(move |_| {
-		let id = params.get().ok().and_then(|x| x.id).unwrap_or_default();
-		let tl_url = format!("{}/context/page", Uri::api(U::Object, &id, false));
+		let tl_url = format!("{}/context/page", Uri::api(U::Object, &id.get(), false));
 		if !feeds.context.next.get_untracked().starts_with(&tl_url) {
 			feeds.context.reset(Some(tl_url));
 		}
 	});
 	view! {
 		<div class="mr-1-r ml-1-r">
-			<Thread tl=feeds.context root=params.get().unwrap().id.unwrap() />
+			<Thread tl=feeds.context root=Uri::full(U::Object, &id.get()) />
 		</div>
 	}
 }
--- web/src/objects/replies.rs
+++ web/src/objects/replies.rs
@@ -7,16 +7,18 @@ use crate::prelude::*;
 pub fn ObjectReplies() -> impl IntoView {
 	let feeds = use_context::<Feeds>().expect("missing feeds context");
 	let params = use_params::<IdParam>();
+	let id = Signal::derive(move ||
+		params.get().ok().and_then(|x| x.id).unwrap_or_default()
+	);
 	create_effect(move |_| {
-		let id = params.get().ok().and_then(|x| x.id).unwrap_or_default();
-		let tl_url = format!("{}/replies/page", Uri::api(U::Object, &id, false));
+		let tl_url = format!("{}/replies/page", Uri::api(U::Object, &id.get(), false));
 		if !feeds.replies.next.get_untracked().starts_with(&tl_url) {
 			feeds.replies.reset(Some(tl_url));
 		}
 	});
 	view! {
 		<div class="mr-1-r ml-1-r">
-			<Thread tl=feeds.replies root=params.get().unwrap().id.unwrap() />
+			<Thread tl=feeds.replies root=Uri::full(U::Object, &id.get()) />
 		</div>
 	}
 }
