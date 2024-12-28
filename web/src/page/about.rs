f445148b9e5eadebb5de709f1d6041bd90501e75
alemi <me@alemi.dev>
2024-12-28 20:19:00 +0100

    fix(web): less outdated stuff just link to site

--- web/src/page/about.rs
+++ web/src/page/about.rs
@@ -9,8 +9,7 @@ pub fn AboutPage() -> impl IntoView {
 				<p><i>"the "<a href="https://en.wikipedia.org/wiki/Fediverse">fediverse</a>" is an ensemble of social networks, which, while independently hosted, can communicate with each other"</i></p>
 				<p>content is aggregated in timelines, logged out users can only access the global server timeline</p>
 				<hr />
-				<p>"while somewhat usable, "<code>μpub</code>" is under active development and still lacks some mainstream features (such as hashtags or lists)"</p>
-				<p>"if you would like to contribute to "<code>μpub</code>"'s development, get in touch and check "<a href="https://github.com/alemidev/upub" target="_blank">github</a>" or "<a href="https://moonlit.technology/alemi/upub.git" target="_blank">forgejo</a></p>
+				<p>"more information on "<a href="https://join.upub.social" target="_blank">join.upub.social</a></p>
 			</div>
 		</div>
 	}
