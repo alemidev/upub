beccf9bc640c8e267a2054b39bbcf6a578e807a6
alemi <me@alemi.dev>
2025-02-20 17:50:40 +0100

    fix(web): activity item component propagate articles

--- web/src/activities/item.rs
+++ web/src/activities/item.rs
@@ -61,7 +61,7 @@ pub fn Item(
 		}
 		match item.object_type().unwrap_or(apb::ObjectType::Object) {
 			// special case for placeholder activities
-			apb::ObjectType::Note | apb::ObjectType::Document(_) =>
+			apb::ObjectType::Note | apb::ObjectType::Document(_) | apb::ObjectType::Article =>
 				Some(view! { <Object object=item.clone() />{sep.clone()} }.into_any()),
 			// everything else
 			apb::ObjectType::Activity(t) => {
