781ea420876d16f882191f3d8817efdf5d8de3de
alemi <me@alemi.dev>
2025-06-18 23:40:12 +0200

    fix(cli): export includes attachments

--- cli/src/export.rs
+++ cli/src/export.rs
@@ -1,5 +1,6 @@
 use futures::TryStreamExt;
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QueryOrder};
+use sea_orm::{ColumnTrait, QueryFilter, QueryOrder};
+use upub::selector::{RichFillable, RichObject};
 
 
 pub async fn export(
@@ -8,18 +9,22 @@ pub async fn export(
 	file: std::path::PathBuf,
 	pretty: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
-
 	let uid = ctx.uid(&actor);
+	let internal = upub::model::actor::Entity::ap_to_internal(&uid, ctx.db())
+		.await?
+		.ok_or(sea_orm::DbErr::RecordNotFound(format!("could not find user {actor}")))?;
+
 	let mut objects = Vec::new();
 	
-	let mut stream = upub::model::object::Entity::find()
+	let mut stream = upub::Query::feed(upub::query_feed_opts!(Some(internal), true))
 		.filter(upub::model::object::Column::AttributedTo.eq(uid))
 		.order_by_asc(upub::model::object::Column::Published)
+		.into_model::<RichObject>()
 		.stream(ctx.db())
 		.await?;
 
 	while let Some(obj) = stream.try_next().await? {
-		objects.push(ctx.ap(obj));
+		objects.push(ctx.ap(obj.load_batched_models(ctx.db()).await?));
 	}
 
 	let writer = std::fs::File::create(file)?;
