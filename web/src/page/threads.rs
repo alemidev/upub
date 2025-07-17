4df2da3de067879ef432cb8a5b69d0522a9782c8
alemi <me@alemi.dev>
2025-07-17 18:00:58 +0200

    fix(web): proper skip/days params

--- web/src/page/threads.rs
+++ web/src/page/threads.rs
@@ -1,5 +1,5 @@
 use leptos::{either::Either, prelude::*};
-use leptos_router::{hooks::{use_navigate, query_signal}, NavigateOptions};
+use leptos_router::hooks::query_signal;
 use crate::prelude::*;
 
 #[component]
@@ -7,8 +7,6 @@ pub fn ThreadsPage() -> impl IntoView {
 	let (days, set_days) = query_signal::<i32>("days");
 	let (skip, set_skip) = query_signal::<i32>("skip");
 	let auth = use_context::<Auth>().expect("missing auth context");
-	let navigate = use_navigate();
-	let _navigate = navigate.clone();
 	view! {
 		{move || if auth.present() {
 			Either::Left(view! {
@@ -35,65 +33,40 @@ pub fn ThreadsPage() -> impl IntoView {
 				<tr>
 					<td class="pa-1">
 						"in last "
-						<input type="number" size="4" placeholder="30"
+						<input type="number" size="4" placeholder="1"
 							prop:value=move || days.get()
 							on:input=move |ev| {
 								ev.prevent_default();
 								let days = event_target_value(&ev).parse().ok();
 								set_days.set(days);
-								navigate(&threads_query_params(days, skip.get()), threads_nav_options());
 						} />
 						" days"
 					</td>
 					<td class="pa-1">
 						"skip "
 						<input type="number" size="4" placeholder="0"
-							prop:value=move || skip.get().unwrap_or(0)
+							prop:value=move || skip.get()
 							on:input=move |ev| {
 								ev.prevent_default();
 								let skip = event_target_value(&ev).parse().ok();
 								set_skip.set(skip);
-								_navigate(&threads_query_params(days.get(), skip), threads_nav_options());
 						} />
 						" most recent days"
 					</td>
 				</tr>
 			</table>
 		</div>
-		{move || match (days.get(), skip.get()) {
-			(Some(days_n), Some(skip_n)) => Either::Right(view! {
+		{move || {
+			let skip_n = skip.get().unwrap_or(0);
+			let days_n = days.get().unwrap_or(1);
+			view! {
 				<div class="mt-3">
 					<Loadable
 						base=format!("{URL_BASE}/threads/page?days={}&skip={skip_n}", days_n + skip_n)
 						element=move |obj| view! { <Item item=obj sep=true /> }
 					/>
 				</div>
-			}),
-			_ => Either::Left(view! {
-				<p class="center"><code>invalid days/skip parameters</code></p>
-			}),
+			}
 		}}
 	}
-}
-
-fn threads_query_params(days: Option<i32>, skip: Option<i32>) -> String {
-	let mut query_string = "/web/threads".to_string();
-	if let Some(days_n) = days {
-		query_string.push('?');
-		query_string.push_str("days=");
-		query_string.push_str(&days_n.to_string());
-	}
-
-	if let Some(skip_n) = skip {
-		query_string.push(if query_string.contains('?') { '&' } else { '?' });
-		query_string.push_str("skip=");
-		query_string.push_str(&skip_n.to_string());
-	}
-
-	query_string
-}
-
-#[inline]
-fn threads_nav_options() -> NavigateOptions {
-	NavigateOptions { resolve: false, replace: true, scroll: false, state: leptos_router::location::State::new(None) }
 }
