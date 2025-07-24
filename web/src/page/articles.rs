d25c5aa2e62b9f6eda2d3439ed488f39dcc6967a
alemi <me@alemi.dev>
2025-07-24 23:58:40 +0200

    fix(web): articles page margin

--- web/src/page/articles.rs
+++ web/src/page/articles.rs
@@ -4,11 +4,9 @@ use crate::prelude::*;
 #[component]
 pub fn ArticlesPage() -> impl IntoView {
 	view! {
-		<div class="mt-3">
-			<Loadable
-				base=format!("{URL_BASE}/articles/page")
-				element=move |obj| view! { <Item item=obj sep=true /> }
-			/>
-		</div>
+		<Loadable
+			base=format!("{URL_BASE}/articles/page")
+			element=move |obj| view! { <Item item=obj sep=true /> }
+		/>
 	}
 }
