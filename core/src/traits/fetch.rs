19548024843f7fa2d4be620535cf3562f7e8670e
alemi <me@alemi.dev>
2026-03-01 12:39:30 +0100

    fix(core): revert to HOST
    some software just has this hardcoded as needed, ughhhhh, annoying!
    hyper/reqwest seem to insert the HOST header themselves, even for POST
    requests, so we can just not have it? dont know

--- core/src/traits/fetch.rs
+++ core/src/traits/fetch.rs
@@ -158,9 +158,9 @@ impl Fetcher for crate::Context {
 			path = path.split('?').next().unwrap_or(path);
 		}
 
-		let mut headers = vec!["(request-target)", "instance", "date"];
+		let mut headers = vec!["(request-target)", "host", "date"];
 		let mut headers_map : BTreeMap<String, String> = [
-			("instance".to_string(), host.clone()),
+			("host".to_string(), host.clone()),
 			("date".to_string(), date.clone()),
 		].into();
 
@@ -186,7 +186,7 @@ impl Fetcher for crate::Context {
 			.request(method, url)
 			.header(ACCEPT, apb::jsonld::ACCEPT_HEADER_ACTIVITY_JSON_LD_JSON)
 			.header(CONTENT_TYPE, apb::jsonld::CONTENT_TYPE_LD_JSON_ACTIVITYPUB)
-			.header("Instance", host.clone())
+			//.header(HOST, host.clone()) // FIXME reqwest/hyper add another HOST header regardless???
 			.header("Date", date.clone())
 			.header("Digest", digest)
 			.header("Signature", signer.header())
